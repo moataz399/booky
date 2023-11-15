ca77aa69bc3dfe09be9cff3b97c42d70f0c4ff7b
moataz399 <moatazzezo41@gmail.com>
2023-11-15 13:03:25 +0200

    adding go router for navigation

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -1,5 +1,4 @@
 
-
 import 'package:booky/features/home/presentation/views/widgets/book_details_view_ody.dart';
 import 'package:flutter/material.dart';
 class BookDetailsView extends StatelessWidget {
--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'best_seller_list_view_item.dart';
 
 class BestSellerListView extends StatelessWidget {

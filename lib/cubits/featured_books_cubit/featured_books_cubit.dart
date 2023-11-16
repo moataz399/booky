b66ce99c4e8c7ab4597bba3fa108d51500d06bc4
moataz399 <moatazzezo41@gmail.com>
2023-11-16 15:03:47 +0200

    create featured books cubit

---
+++ lib/cubits/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:booky/features/home/data/models/BookModel.dart';
+import 'package:meta/meta.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
